7c19aa0588aa924acaad3a08a720efc72b295eed
YoungwanCho <developer119korea@gmail.com>
2020-04-04 20:46:41 +0900

    움직일 포인트를 배치하고 기즈모를 보여줌

---
+++ Assets/Scripts/Rail.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class Rail : MonoBehaviour
+{
+    private Transform[] nodes = null;
+
+    private void Start()
+    {
+        nodes = GetComponentsInChildren<Transform>();
+    }
+
+    private void OnDrawGizmos()
+    {
+        for (int i=0; i<nodes.Length -1; i ++)
+        {
+            Handles.DrawDottedLine(nodes[i].position, nodes[i + 1].position, 3.0f);
+        }
+    }
+}
