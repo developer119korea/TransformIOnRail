cfc869ae70977ef31116db0795b85bbb942c6540
YoungwanCho <developer119korea@gmail.com>
2020-04-04 21:11:43 +0900

    카메라가 포인트를 따라 이동하면서 포인트가 바라 보는 방향을 바라보도록 기능 추가
    - 유니티 에디어에서 포인트(노드)를 배치 할 때 cmd + f 버튼을 통해 현재 에디터 카메라가 보라보고 있는 시점을 복사 해서 대입 할 수 있다

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] private Rail rail;
+
+    private int currentSeg;
+    private float transition;
+    private bool isCompleted;
+
+    private void Update()
+    {
+        if (!rail) return;
+
+        if (!isCompleted) Play();
+    }
+
+    private void Play()
+    {
+        transition += Time.deltaTime * 1 / 2.5f;
+        if (transition > 1)
+        {
+            transition = 0;
+            currentSeg++;
+        }
+        else if (transition < 0)
+        {
+            transition = 1;
+            currentSeg--;
+        }
+
+        transform.position = rail.LinearPosition(currentSeg, transition);
+        transform.rotation = rail.Orientation(currentSeg, transition);
+    }
+}
--- Assets/Scripts/Rail.cs
+++ Assets/Scripts/Rail.cs
@@ -1,7 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using UnityEditor;
 using UnityEngine;
-using UnityEditor;
 
 [ExecuteInEditMode]
 public class Rail : MonoBehaviour
@@ -13,9 +11,23 @@ public class Rail : MonoBehaviour
         nodes = GetComponentsInChildren<Transform>();
     }
 
+    public Vector3 LinearPosition(int seg, float ratio)
+    {
+        Vector3 p1 = nodes[seg].position;
+        Vector3 p2 = nodes[seg + 1].position;
+        return Vector3.Lerp(p1, p2, ratio);
+    }
+
+    public Quaternion Orientation(int seg, float ratio)
+    {
+        Quaternion q1 = nodes[seg].rotation;
+        Quaternion q2 = nodes[seg + 1].rotation;
+        return Quaternion.Lerp(q1, q2, ratio);
+    }
+
     private void OnDrawGizmos()
     {
-        for (int i=0; i<nodes.Length -1; i ++)
+        for (int i = 0; i < nodes.Length - 1; i++)
         {
             Handles.DrawDottedLine(nodes[i].position, nodes[i + 1].position, 3.0f);
         }
